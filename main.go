1673bea8e768f74c6a61ea698950a3c09e881a56
Bartłomiej Rudecki <ejbtrd@gmail.com>
2023-02-23 01:17:38 +0100

    yellope: Download latest platform tools to C:\platform-tools

--- main.go
+++ main.go
@@ -2,8 +2,44 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
+	"os"
 )
+
+func download(path string, url string) error {
+	resp, err := http.Get(url)
+
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	out, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	return err
+}
 
 func main() {
 	fmt.Println("Installing latest google platform tools...")
+
+	const PLATFORM_TOOLS_PATH string = "C:\\platform-tools\\"
+
+	// Create inital directory
+	os.Mkdir(PLATFORM_TOOLS_PATH, os.ModePerm)
+
+	const PLATFORM_TOOLS_URL string = "https://dl.google.com/android/repository/platform-tools-latest-windows.zip"
+
+	// Download platform tools archive
+	download(PLATFORM_TOOLS_PATH+"platform-tools.zip", PLATFORM_TOOLS_URL)
+
+	fmt.Println("All done! Your platform tools are installed to C:\\platform-tools!")
+	fmt.Println("Press any key to exit...")
+	fmt.Scanln()
 }
